1b77d195b3e3c9230c86dc0f19031a6882ce5224
Genji-MS <Genji.Tapia@students.makeschool.com>
2020-06-01 12:40:02 -0700

    View events setup

--- app.js
+++ app.js
@@ -2,15 +2,23 @@
 const express = require('express')
 const app = express()
 
-// Tell our app to send the "hello world" message to our home page
+// INDEX
 app.get('/', (req, res) => {
   //res.send('Hello Peanut!')
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  //res.render('home', { msg: 'Handlebars are Cool!' });
+  res.render('events-index', { events: events });
 })
+
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+    { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+  ]
+
 
 // require handlebars
 var exphbs = require('express-handlebars');
-
 // Use "main" as our default layout
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 // Use handlebars to render
